3ca32d6d4ede6f9d36e6a6b5428fc7ac8820b8ac
Tri Nguyen <mankinchi.1995@gmail.com>
2020-08-29 00:34:53 +1000

    add db helper file

---
+++ assets/js/db.js
@@ -0,0 +1,30 @@
+/* global firebase */
+/* eslint no-unused-vars: "off" */
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDrYYGZUME97OexecodMvSfP6rRw37D1tk',
+	authDomain: 'medusa-branches.firebaseapp.com',
+	databaseURL: 'https://medusa-branches.firebaseio.com',
+	projectId: 'medusa-branches',
+	storageBucket: 'medusa-branches.appspot.com',
+	messagingSenderId: '621991997799',
+	appId: '1:621991997799:web:fe8fc929e9d7e5ef683fc2',
+	measurementId: 'G-96BMDM9343',
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+const getRef = (route) => db.ref(route);
+
+const getDataOnce = (route, callback) => {
+	getRef(route).once('value', (snapshot) => {
+		callback(snapshot.val());
+	});
+};
+
+const getData = (route, callback) => {
+	getRef(route).on('value', (snapshot) => {
+		callback(snapshot.val());
+	});
+};
