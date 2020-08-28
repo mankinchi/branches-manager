8872419c7464c72e2d532e18fdd38d6315175993
Tri Nguyen <mankinchi.1995@gmail.com>
2020-08-29 01:59:38 +1000

    add js for add

--- assets/js/add.js
+++ assets/js/add.js
@@ -1,8 +1,9 @@
 /* global $ */
-/* global getDataOnce, getRef */
+/* global getData, getRef, populateIssuesForElement */
 
-getDataOnce('/users/tri', ({ info: _info, ...servers }) => {
-	const $servers = $('#servers');
+getData('/users/tri', ({ info, ...servers }) => {
+	const $servers = $('.servers-selector');
+	$servers.empty();
 	for (const server of Object.keys(servers)) {
 		$servers.append(
 			$('<option>', {
@@ -11,10 +12,25 @@ getDataOnce('/users/tri', ({ info: _info, ...servers }) => {
 			}),
 		);
 	}
+
+	populateIssuesForElement(
+		$('.issues-selector'),
+		(issueId, {
+			name,
+			branch,
+		}) => (
+			$('<option>', {
+				text: name,
+				'data-branch': branch,
+				'data-issueid': issueId,
+			})
+		),
+		info,
+	);
 });
 
 $('#add-issue').click(() => {
-	const server = $('#servers').val();
+	const server = $('#server-new-issue').val();
 	const issueName = $('#issue-name').val();
 	const issueLink = $('#issue-link').val();
 	const issueBranch = $('#issue-branch').val();
@@ -38,4 +54,24 @@ $('#add-issue').click(() => {
 
 	$('#issue-name, #issue-link, #issue-branch').val('');
 	$('#set-current-branch').prop('checked', false);
+});
+
+$('#add-server').click(() => {
+	const serverName = $('#server-name').val();
+	getRef('/users/tri').update({
+		[serverName]: {
+			issue: '',
+		},
+	});
+
+	$('#server-name').val('');
+});
+
+$('#set-issue-for-server').click(() => {
+	const server = $('#server-set-issue').val();
+	const issue = $('#issue-for-server').find('option:selected').data('issueid');
+
+	getRef(`/users/tri/${server}`).set({
+		issue,
+	});
 });
