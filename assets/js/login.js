c5c7f114294c4bcaa137e80e48865149ec470c27
Tri Nguyen <mankinchi.1995@gmail.com>
2020-09-01 23:35:49 +1000

    add login page

---
+++ assets/js/login.js
@@ -0,0 +1,65 @@
+/* global $, auth */
+/* global getRef */
+
+const getLoginCredential = () => ({
+	email: $('#email').val(),
+	password: $('#password').val(),
+	name: $('#name').val(),
+});
+
+const showErrorMessage = (errorMessage) => {
+	$('#alert-error').text(errorMessage).fadeIn('fast');
+};
+
+const showSuccessMessage = () => {
+	$('#alert-success').fadeIn('fast');
+};
+
+const hideErrorMessage = () => {
+	$('.alert').fadeOut('fast');
+};
+
+$('.btn').click(async (e) => {
+	hideErrorMessage();
+	const $button = $(e.currentTarget);
+
+	const credential = getLoginCredential();
+	if (!credential.email && !credential.password && !credential.email) {
+		showErrorMessage('Please fill all data in the form');
+		return;
+	}
+
+	try {
+		if ($button.attr('id') === 'sign-up') {
+			const { user } = await auth.createUserWithEmailAndPassword(
+				credential.email,
+				credential.password,
+			);
+
+			await getRef('/users').push({
+				info: {
+					email: credential.email,
+					name: credential.name,
+					id: user.uid,
+				},
+			});
+
+			showSuccessMessage();
+		} else {
+			await auth.signInWithEmailAndPassword(
+				credential.email,
+				credential.password,
+			);
+
+			showSuccessMessage();
+		}
+	} catch (error) {
+		showErrorMessage(error.message);
+	}
+});
+
+auth.onAuthStateChanged((user) => {
+	if (user) {
+		window.location.href = '/';
+	}
+});
