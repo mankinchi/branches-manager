a7aa1399b981934257b88caddc820adf3b386969
Tri Nguyen <mankinchi.1995@gmail.com>
2020-09-01 23:36:39 +1000

    update server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+const db = require('./database');
+
 const app = express();
 const port = 3000;
 
 app.use(express.static('assets'));
+app.use(express.json());
 
 app.get('/', (_req, res) => {
 	res.sendFile(path.join(__dirname, 'assets', 'view', 'index.html'));
@@ -12,6 +15,36 @@ app.get('/', (_req, res) => {
 
 app.get('/add', (_req, res) => {
 	res.sendFile(path.join(__dirname, 'assets', 'view', 'add.html'));
+});
+
+app.get('/login', (_req, res) => {
+	res.sendFile(path.join(__dirname, 'assets', 'view', 'login.html'));
+});
+
+app.post('/api/change-issue', (req, res) => {
+	const {
+		server,
+		branch,
+	} = req.body;
+
+	db.ref('/issues').once('value', (snapshot) => {
+		const issues = snapshot.val();
+
+		let toBeUpdatedIssueId = '';
+		for (const [issueId, { branch: issueBranch }] of Object.entries(issues)) {
+			if (issueBranch === branch) {
+				toBeUpdatedIssueId = issueId;
+				break;
+			}
+		}
+
+		db.ref(`/users/tri/${server}`).update({
+			issue: toBeUpdatedIssueId,
+		});
+	});
+	res.status(200).json({
+		a: 1,
+	});
 });
 
 app.listen(port, () => {
