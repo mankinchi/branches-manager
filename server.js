251a013978df421ecda0bd031dec33d81b449fde
Tri Nguyen <mankinchi.1995@gmail.com>
2020-09-03 23:24:10 +1000

    allow custom port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const db = require('./database');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('assets'));
 app.use(express.json());
