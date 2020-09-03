84afeb00798c4ccab2dbf2c0f651b834caf473bb
Tri Nguyen <mankinchi.1995@gmail.com>
2020-09-03 21:59:47 +1000

    add comments

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ const serviceAccount = {
 	type: process.env.TYPE,
 	project_id: process.env.PROJECT_ID,
 	private_key_id: process.env.PRIVATE_KEY_ID,
+	// Have to replace \n again with private key
 	private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
 	client_email: process.env.CLIENT_EMAIL,
 	client_id: process.env.CLIENT_ID,
